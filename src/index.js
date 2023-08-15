ddfcfef45a2efd3f8d73e1488061d31d262ed7e8
cherkasovaa <alina.ch1202@gmail.com>
2023-08-15 12:39:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const obj = bracketsConfig.reduce((o, [k, v]) => Object.assign(o, { [k]: v }), {});
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (char in obj && !stack.includes(char)) {
+      stack.push(char);
+    } else {
+      obj[stack[stack.length - 1]] !== char ? stack.push(char) : stack.pop();
+    }
+  }
+
+  return !stack.length;
+};
